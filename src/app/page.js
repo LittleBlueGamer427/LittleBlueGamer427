2b57c4a0ed1842a705b57f8fb4fc5f7cc14ebc70
LittleBlueGamer <148423893+LittleBlueGamer427@users.noreply.github.com>
2025-01-26 00:57:25 -0800

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -17,7 +17,7 @@ export default function Home() {
           <img src="avatarHead.png" width="70" height="70" />
           <ul>
             <li><Link href="/pages/learn/learnmain">LEARN</Link></li>
-            <li><Link href="/pages/plannedstreams">PLANNED STREAMS</Link></li>
+            <li><Link href="https://calendar.google.com/calendar/embed?src=3da94c8464b66e22c1d9d2ea957934a1cb214741506512c1b36c1924e2b2f9e3%40group.calendar.google.com&ctz=America%2FLos_Angeles">PLANNED STREAMS</Link></li>
             <li><a href="/mewo">404</a></li>
           </ul>
         </div>
